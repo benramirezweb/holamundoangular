cd591994d2c4b52649c4894dc2255f5c032fda81
benramirezweb <benjamin.dg@gmail.com>
2018-10-11 12:11:47 -0500

    Actualización de estilos y componentes HEADER, FOOTER y BODY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { FooterComponent } from './footer.component';
+import { BodyComponent } from './body.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
